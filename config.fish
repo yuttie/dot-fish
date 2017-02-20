ae285957d2bd2790e9552ba09672048c4837f716
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2017-02-21 00:07:19 +0900

    Set indent size to 2

--- config.fish
+++ config.fish
@@ -1,12 +1,12 @@
 set fish_greeting
 
 if status --is-login
-    set PATH $PATH /usr/bin /sbin
+  set PATH $PATH /usr/bin /sbin
 end
 
 # https://bugs.funtoo.org/browse/FL-2006
 if status --is-interactive
-    keychain --eval --quiet --quick id_ed25519 id_rsa_4096 | source
+  keychain --eval --quiet --quick id_ed25519 id_rsa_4096 | source
 end
 
 # OPAM configuration
