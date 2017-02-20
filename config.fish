37d1630713dd64cb5103ddb51d8cb64d9871de2b
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2017-02-21 00:02:56 +0900

    Add configuration for OPAM

--- config.fish
+++ config.fish
@@ -8,3 +8,6 @@ end
 if status --is-interactive
     keychain --eval --quiet --quick id_ed25519 id_rsa_4096 | source
 end
+
+# OPAM configuration
+source /home/yuta/.opam/opam-init/init.fish > /dev/null 2> /dev/null or true
