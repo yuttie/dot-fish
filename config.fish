9768005676ae26ada82d103c98ca28e906449f10
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2017-02-21 13:53:10 +0900

    Set some environment variables

--- config.fish
+++ config.fish
@@ -2,6 +2,11 @@ set fish_greeting
 set -g fish_prompt_pwd_dir_length 0
 set -g theme_color_scheme terminal2
 
+# Set variables as global instead of universal because they have system-wide values
+set -gx EDITOR nvim
+set -gx LESS '--RAW-CONTROL-CHARS --ignore-case --chop-long-lines'
+set -gx GPG_TTY (tty)
+
 if test -f ~/.dir_colors
   eval (dircolors -c ~/.dir_colors)
 else
