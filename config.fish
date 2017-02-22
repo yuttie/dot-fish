22b2519b8864f8386d4ef9436db714c62c692ba5
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2017-02-22 11:18:58 +0900

    Delete configuration for OPAM, which depends on an environment

--- config.fish
+++ config.fish
@@ -26,6 +26,3 @@ end
 if status --is-interactive
   keychain --eval --quiet --quick id_ed25519 id_rsa_4096 | source
 end
-
-# OPAM configuration
-source /home/yuta/.opam/opam-init/init.fish > /dev/null 2> /dev/null or true
