d24b4001ccd978ced144ab90af6dbbda25fdd5e1
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2017-02-24 18:05:02 +0900

    Use hybrid key bindings

--- config.fish
+++ config.fish
@@ -15,14 +15,21 @@ end
 
 # Key bindings
 function fish_user_key_bindings
-  fish_vi_key_bindings
-
   bind \ct '__fzf_find_file'
   bind \cr '__fzf_reverse_isearch'
   bind \ex '__fzf_find_and_execute'
   bind \ed '__fzf_cd'
   bind \eD '__fzf_cd_with_hidden'
+
+  if bind -M insert >/dev/null ^/dev/null
+    bind -M insert \ct '__fzf_find_file'
+    bind -M insert \cr '__fzf_reverse_isearch'
+    bind -M insert \ex '__fzf_find_and_execute'
+    bind -M insert \ed '__fzf_cd'
+    bind -M insert \eD '__fzf_cd_with_hidden'
+  end
 end
+fish_hybrid_key_bindings
 
 # https://bugs.funtoo.org/browse/FL-2006
 if status --is-interactive
