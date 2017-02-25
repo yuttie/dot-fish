b7ff1f55bbdd43bfd3bd1c866376956f2064c01c
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2017-02-25 23:31:11 +0900

    Fix invalid key binding to __fzf_reverse_isearch_zsh

--- config.fish
+++ config.fish
@@ -17,7 +17,7 @@ end
 function fish_user_key_bindings
   bind \ct '__fzf_find_file'
   bind \cr '__fzf_reverse_isearch'
-  bind \cR '__fzf_reverse_isearch_zsh'
+  bind \er '__fzf_reverse_isearch_zsh'
   bind \ex '__fzf_find_and_execute'
   bind \ed '__fzf_cd'
   bind \eD '__fzf_cd_with_hidden'
@@ -25,7 +25,7 @@ function fish_user_key_bindings
   if bind -M insert >/dev/null ^/dev/null
     bind -M insert \ct '__fzf_find_file'
     bind -M insert \cr '__fzf_reverse_isearch'
-    bind -M insert \cR '__fzf_reverse_isearch_zsh'
+    bind -M insert \er '__fzf_reverse_isearch_zsh'
     bind -M insert \ex '__fzf_find_and_execute'
     bind -M insert \ed '__fzf_cd'
     bind -M insert \eD '__fzf_cd_with_hidden'
