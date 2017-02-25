a30933ba969efa344e6d81db24ef10d1a6b6b32e
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2017-02-25 23:26:28 +0900

    Honor $XDG_CONFIG_HOME

--- functions/__fzf_reverse_isearch_zsh.fish
+++ functions/__fzf_reverse_isearch_zsh.fish
@@ -1,5 +1,8 @@
 function __fzf_reverse_isearch_zsh
-  python ~/.config/fish/scripts/zsh_history.py | tac | __fzfcmd +s --tiebreak=index --toggle-sort=ctrl-r -q (commandline) $FZF_REVERSE_ISEARCH_OPTS | read -l select
+  set -l config_home $XDG_CONFIG_HOME
+  test -z "$config_home"; and set -l config_home ~/.config
+
+  python $config_home/fish/scripts/zsh_history.py | tac | __fzfcmd +s --tiebreak=index --toggle-sort=ctrl-r -q (commandline) $FZF_REVERSE_ISEARCH_OPTS | read -l select
   if not test -z $select
     commandline -rb (printf $select)
     commandline -f repaint
