b7d03ac69640276597057d455ada889c4f5c0df1
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2017-02-26 11:57:13 +0900

    Fix for histories containing '%'

--- functions/__fzf_reverse_isearch_zsh.fish
+++ functions/__fzf_reverse_isearch_zsh.fish
@@ -4,7 +4,7 @@ function __fzf_reverse_isearch_zsh
 
   python $config_home/fish/scripts/zsh_history.py | tac | __fzfcmd +s --tiebreak=index --toggle-sort=ctrl-r -q (commandline) $FZF_REVERSE_ISEARCH_OPTS | read -l select
   if not test -z $select
-    commandline -rb (printf $select)
+    commandline -rb "$select"
     commandline -f repaint
   end
 end
