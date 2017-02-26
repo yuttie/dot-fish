065ce2ea65dccd8a955b4cb7ec3c4d4171ab9063
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2017-02-27 08:48:35 +0900

    Configure key bindings only in the interactive mode

--- config.fish
+++ config.fish
@@ -13,27 +13,27 @@ else
   eval (dircolors -c /etc/DIR_COLORS)
 end
 
-# Key bindings
-function fish_user_key_bindings
-  bind \ct '__fzf_find_file'
-  bind \cr '__fzf_reverse_isearch'
-  bind \er '__fzf_reverse_isearch_zsh'
-  bind \ex '__fzf_find_and_execute'
-  bind \ed '__fzf_cd'
-  bind \eD '__fzf_cd_with_hidden'
-
-  if bind -M insert >/dev/null ^/dev/null
-    bind -M insert \ct '__fzf_find_file'
-    bind -M insert \cr '__fzf_reverse_isearch'
-    bind -M insert \er '__fzf_reverse_isearch_zsh'
-    bind -M insert \ex '__fzf_find_and_execute'
-    bind -M insert \ed '__fzf_cd'
-    bind -M insert \eD '__fzf_cd_with_hidden'
-  end
-end
-fish_hybrid_key_bindings
-
 if status --is-interactive
+  # Key bindings
+  function fish_user_key_bindings
+    bind \ct '__fzf_find_file'
+    bind \cr '__fzf_reverse_isearch'
+    bind \er '__fzf_reverse_isearch_zsh'
+    bind \ex '__fzf_find_and_execute'
+    bind \ed '__fzf_cd'
+    bind \eD '__fzf_cd_with_hidden'
+
+    if bind -M insert >/dev/null ^/dev/null
+      bind -M insert \ct '__fzf_find_file'
+      bind -M insert \cr '__fzf_reverse_isearch'
+      bind -M insert \er '__fzf_reverse_isearch_zsh'
+      bind -M insert \ex '__fzf_find_and_execute'
+      bind -M insert \ed '__fzf_cd'
+      bind -M insert \eD '__fzf_cd_with_hidden'
+    end
+  end
+  fish_hybrid_key_bindings
+
   # https://bugs.funtoo.org/browse/FL-2006
   keychain --eval --quiet --quick id_ed25519 id_rsa_4096 | source
 end
