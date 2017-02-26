aad8ea608bd9855f1779b6635b144e594a41d850
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2017-02-27 08:43:28 +0900

    Move a comment to a proper position

--- config.fish
+++ config.fish
@@ -33,7 +33,7 @@ function fish_user_key_bindings
 end
 fish_hybrid_key_bindings
 
-# https://bugs.funtoo.org/browse/FL-2006
 if status --is-interactive
+  # https://bugs.funtoo.org/browse/FL-2006
   keychain --eval --quiet --quick id_ed25519 id_rsa_4096 | source
 end
