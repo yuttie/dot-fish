faed13bb0657341b17e7d0a27c8d7c9d9d1ab860
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2017-08-21 01:27:41 +0900

    Add directories immediately under the root dir as completion cadidates

--- completions/note.fish
+++ completions/note.fish
@@ -1,1 +1,2 @@
-complete -c note -x -d 'note' -a '(sh -c "cd $NOTE_ROOT_DIR; find . -regextype posix-extended -not -path \'*/\.*\' -iregex \'$NOTE_COMPLETION_REGEX\' | cut -c 3-")'
+complete -c note -x -d 'note'     -a '(sh -c "cd $NOTE_ROOT_DIR; find . -regextype posix-extended -not -path \'*/\.*\' -type f -iregex \'$NOTE_COMPLETION_REGEX\' | cut -c 3-")'
+complete -c note -x -d 'note-dir' -a '(sh -c "cd $NOTE_ROOT_DIR; find . -maxdepth 1               -not -path \'*/\.*\' -type d                                    | cut -c 3-")'
