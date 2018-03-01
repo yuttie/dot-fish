9b9fc227bf0e0ea5be1b011df0bec5b05466b486
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2018-03-01 11:15:08 +0900

    Fix duplicated prompt by setting PIPENV_SHELL_FANCY to 1

--- init.fish
+++ init.fish
@@ -16,6 +16,7 @@ set -Ux LESS_TERMCAP_ue \e'[0m'          # end underline
 set -l additional_paths
 set -Ux BC_ENV_ARGS "--mathlib $HOME/.bc"
 set -Ux PYTHONSTARTUP "$HOME/.pythonstartup"
+set -Ux PIPENV_SHELL_FANCY 1
 set -Ux RUST_SRC_PATH "$HOME/.rustup/toolchains/stable-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/src"
 set -l additional_paths ~/.gem/ruby/*/bin $additional_paths
 set -l additional_paths ~/.cargo/bin $additional_paths
