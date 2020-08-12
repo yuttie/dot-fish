7a895beba28ea187b23843e293409e0f38d7cf13
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2020-08-12 21:34:56 +0900

    Update keybindings for fzf
    Updated according to https://github.com/jethrokuan/fzf/blob/b7f79784c2695c222c8e5a1e55912d6b515fae09/conf.d/fzf_key_bindings.fish

--- config.fish
+++ config.fish
@@ -27,20 +27,18 @@ if status --is-interactive
 
   # Key bindings
   function fish_user_key_bindings
-    bind \ct '__fzf_find_file'
+    bind \co '__fzf_find_file'
     bind \cr '__fzf_reverse_isearch'
     bind \er '__fzf_reverse_isearch_zsh'
-    bind \ex '__fzf_find_and_execute'
-    bind \ed '__fzf_cd'
-    bind \eD '__fzf_cd_with_hidden'
+    bind \ec '__fzf_cd'
+    bind \eC '__fzf_cd --hidden'
 
-    if bind -M insert >/dev/null ^/dev/null
-      bind -M insert \ct '__fzf_find_file'
-      bind -M insert \cr '__fzf_reverse_isearch'
-      bind -M insert \er '__fzf_reverse_isearch_zsh'
-      bind -M insert \ex '__fzf_find_and_execute'
-      bind -M insert \ed '__fzf_cd'
-      bind -M insert \eD '__fzf_cd_with_hidden'
+    if bind -M insert >/dev/null 2>/dev/null
+        bind -M insert \co '__fzf_find_file'
+        bind -M insert \cr '__fzf_reverse_isearch'
+        bind -M insert \er '__fzf_reverse_isearch_zsh'
+        bind -M insert \ec '__fzf_cd'
+        bind -M insert \eC '__fzf_cd --hidden'
     end
   end
   fish_hybrid_key_bindings
