60604e375166b11dd870ecb08151ad2e080b53e3
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2021-08-07 15:56:07 +0900

    Support stacking and restoring multiple commandlines

--- functions/push-line.fish
+++ functions/push-line.fish
@@ -7,17 +7,24 @@
 # end
 
 function push-line
-  set cl (commandline)
-  commandline -f repaint
-  if test -n (string join $cl)
-    set -g fish_buffer_stack $cl
-    commandline ''
-    commandline -f repaint
-
-    function restore_line -e fish_postexec
-      commandline $fish_buffer_stack
-      functions -e restore_line
-      set -e fish_buffer_stack
+  # Add an event handler for fish_postexec
+  if test (count $fish_buffer_stack) -eq 0
+    function _push-line_restore_line -e fish_postexec
+      # Pop a commandline from the stack if possible
+      if test (count $fish_buffer_stack) -gt 0
+        commandline $fish_buffer_stack[-1]
+        set -e fish_buffer_stack[-1]
+      end
+      # Unregister the event handler if nothing left in the stack
+      if test (count $fish_buffer_stack) -eq 0
+        functions -e _push-line_restore_line
+      end
     end
   end
+
+  # Push the current commandline
+  set -g fish_buffer_stack $fish_buffer_stack (commandline)
+  # Clear the command line
+  commandline ''
+  commandline -f repaint
 end
