a1c70a08aace5582f79c5dc8bac8863101176b74
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2022-05-29 11:53:06 +0900

    Don't use prefix search

--- functions/ltma.fish
+++ functions/ltma.fish
@@ -1,7 +1,7 @@
 function ltma --argument-names session
   if test -n "$session"
-    tmux has-session -t $session
-    and tmux attach-session -t $session $argv[2..-1]
+    tmux has-session -t="$session"
+    and tmux attach-session -t="$session" $argv[2..-1]
     or tmux new-session -s $session $argv[2..-1]
   else
     echo Session name must be specified.
