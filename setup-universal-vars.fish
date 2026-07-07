53e311d7f3a5f681b3e2dc74c70fbb47cb6cbd5f
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2026-07-07 12:19:02 +0900

    refactor: remove env vars for GUI apps
    These variables are now defined in the Hyprland config.

--- setup-universal-vars.fish
+++ setup-universal-vars.fish
@@ -65,8 +65,6 @@ set -U __done_exclude 'git (?!push|pull|fetch)|^ '
 # Others
 set -U DICTIONARY 'en_US'
 set -U FZF_DEFAULT_OPTS '--no-separator --reverse --color=fg:-1,bg:-1,fg+:15,bg+:0,hl:6,hl+:14,prompt:10,pointer:9,info:4,marker:9,spinner:9,header:9'
-set -U OOO_DISABLE_RECOVERY 1
-set -U GQ_NEW_INSTANCE yes
 
 
 # Use default values
